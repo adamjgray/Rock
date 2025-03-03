3a5438204c3d1ff1716cea0563309b4ed7d16fab
Braden Cohen <braden@triumph.tech>
2025-03-03 09:24:02 -0700

    - Update to use new methods in develop.

--- Rock.Blocks/Communication/Chat/GeneralChat.cs
+++ Rock.Blocks/Communication/Chat/GeneralChat.cs
@@ -1,32 +1,28 @@
 ﻿using Rock.Communication.Chat;
 using Rock.Model;
-
-using System.Collections.Generic;
 using Rock.SystemGuid;
 
 using System.ComponentModel;
 using System.Threading.Tasks;
-using System.Linq;
 using Rock.Web.Cache;
 
 namespace Rock.Blocks.Communication.Chat
 {
     [DisplayName( "Chat" )]
     [Category( "Chat" )]
-    [Description( "" )]
+    [Description( "Integrate StreamChat into your Rock Mobile application." )]
     [SupportedSiteTypes( SiteType.Mobile )]
 
     [Rock.SystemGuid.EntityTypeGuid( "B3D6F875-1589-4543-9E76-5C41201B465B" )]
     [Rock.SystemGuid.BlockTypeGuid( "723A3F70-87DC-4BA0-A6FB-0AC15B1865B0" )]
     public class GeneralChat : RockBlockType
     {
-        public override object GetMobileConfigurationValues()
-        {
-            return base.GetMobileConfigurationValues();
-        }
-
         #region Block Actions
 
+        /// <summary>
+        /// Gets the chat data.
+        /// </summary>
+        /// <returns></returns>
         [BlockAction]
         public async Task<BlockActionResult> GetChatData()
         {
@@ -47,18 +43,16 @@ namespace Rock.Blocks.Communication.Chat
 
             using ( var chatHelper = new ChatHelper() )
             {
-                var chatToken = await chatHelper.GetChatUserTokenAsync( person.Id );
-                var chatUserKey = chatHelper.GetRockChatUserPersonKeys( new List<int> { person.Id } ).FirstOrDefault()?.ChatUserKey;
-
+                var chatUserAuth = await chatHelper.GetChatUserAuthenticationAsync( person.Id );
                 var sharedChannelGroupStreamKey = ChatHelper.GetChatChannelTypeKey( sharedChannelGroupTypeId.Value );
                 var directMessagingChannelStreamKey = ChatHelper.GetChatChannelTypeKey( directMessagingChannelGroupTypeId.Value );
 
                 return ActionOk( new
                 {
-                    Token = chatToken,
-                    UserId = chatUserKey,
+                    Token = chatUserAuth.Token,
+                    UserId = chatUserAuth.ChatUserKey,
 
-                    // BC TODO: Should probably be passing these through the update package
+                    // TODO: Should probably be passing these through the update package
                     // instead of here.
                     SharedChannelStreamKey = sharedChannelGroupStreamKey,
                     DirectMessagingChannelStreamKey = directMessagingChannelStreamKey
@@ -66,10 +60,6 @@ namespace Rock.Blocks.Communication.Chat
             }
         }
         
-        #endregion
-
-        #region Helper Classes
-
         #endregion
     }
 }
